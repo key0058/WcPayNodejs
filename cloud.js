f6ff2b164f478d897d051bfb4a93e43bcb2010be
key0058 <thekey0058@163.com>
2018-03-22 10:09:27 +0800

    Update test create order

--- cloud.js
+++ cloud.js
@@ -56,3 +56,91 @@ AV.Cloud.define('order', (request, response) => {
     response.error(error);
   });
 });
+
+
+AV.Cloud.define('createWechatPayment', function(request, response) {
+	
+  var data = new Date();
+  
+  var appId = 'wx9eab9f19e4e05ff6';
+  var mchId = '1481054882';
+  var deviceInfo = 'WEB';
+  var nonceStr = 'ec2316275641faa3aacf3cc599e8730f';
+  var sign = '';
+  var signType = 'MD5';
+  var body = 'LeanCloud-小程序支付测试';
+  var detail = '';
+  var attach = '';
+  var outTradeNo = data.getTime() + "";
+  var feeType = 'CNY';
+  var totalFee = 1;
+  var timeExpire = '';
+  var goodsTag = '';
+  var notifyUrl = 'https://stg-key0058.leanapp.cn.leanapp.cn/weixin/pay-callback';
+  var tradeType = 'JSAPI';
+  var productId = '';
+  var limitPay = '';
+  var spBillCreateIp = request.meta.remoteAddress;
+  var openId = request.currentUser.get('authData').lc_weapp.openid;
+  var key = 'evtrjds8bq5eptw8sj8w51a2xpw18o7v';
+  
+  var xml2js = require("xml2js");
+  var crypto = require("crypto");
+  var md5 = crypto.createHash('md5');
+  
+  nonceStr = crypto.randomBytes(16).toString('hex');
+  
+  var tempSign = "appid=" + appId + "&body=" + body + "&device_info=" + deviceInfo;
+  tempSign = tempSign + "&mch_id=" + mchId + "&nonce_str=" + nonceStr + "&notify_url=" + notifyUrl;
+  tempSign = tempSign + "&openid=" + openId + "&out_trade_no=" + outTradeNo + "&spbill_create_ip=" + spBillCreateIp + "&total_fee=" + totalFee + "&trade_type=" + tradeType;
+  tempSign = tempSign + "&key=" + key;
+
+  console.log(tempSign);
+
+  md5.update(tempSign);
+    sign = crypto.createHash('md5').update(tempSign).digest('hex').toUpperCase();
+    
+    var tempXml = {
+        xml: {
+            appid: appId,
+            body: body,
+            mch_id: mchId,
+            device_info: deviceInfo,
+            nonce_str: nonceStr,
+            notify_url: notifyUrl,
+            openid: openId,
+            out_trade_no: outTradeNo,
+            spbill_create_ip: spBillCreateIp,
+            total_fee: totalFee,
+            trade_type: tradeType,
+            sign: sign
+        }
+    }
+    
+    var builder = new xml2js.Builder();
+    var xml = builder.buildObject(tempXml);
+    
+  console.log("XML content: " + xml);
+    
+  var httpRequest = require("request");	
+  var options = {
+    method: "POST",
+    uri: "https://api.mch.weixin.qq.com/pay/unifiedorder",
+    body: xml,
+    json: true
+  };
+  httpRequest(options, function(error, res, body){  
+    xml2js.parseString(res.body, {explicitArray : false}, function(err, json) {
+      var result = JSON.parse(JSON.stringify(json));
+      if (result.xml.return_code = 'SUCCESS') {
+        console.log("Response body: " + body);
+        console.log("Create payment id[" + result.xml.prepay_id + "]");
+        response.success({"prepayId": result.xml.prepay_id});
+      } else {
+        response.success('ERROR: ' + result);
+      }
+    });
+  });  
+  
+});
+
